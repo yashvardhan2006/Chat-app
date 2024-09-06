877583f74f510d01e1fbc4df6e7016a64d227eca
yashvardhan2006 <yvardhansolanki@gmail.com>
2024-09-07 05:16:35 +0530

    Deploying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Toaster } from 'react-hot-toast';
 import Login from './pages/login/Login.jsx'
 import SignUp from './pages/signup/SignUp.jsx'
 import "./App.css";
-import Home from './pages/home/home.jsx'
+import Home from './pages/home/Home.jsx'
 import { useAuthContext } from "./context/AuthContext.jsx";
 function App() {
   const { authUser } = useAuthContext();
