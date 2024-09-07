bd47a899bcca13e256d62bcd528e4dd76ae39a39
yashvardhan2006 <yvardhansolanki@gmail.com>
2024-09-07 05:52:26 +0530

    Deploying

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ const [onlineUsers, setonlineUsers] = useState([])
 const {authUser} = useAuthContext()
 useEffect(() => {
 if(authUser){
-    const socket = io("http://localhost:8000",{
+    const socket = io("https://chat-app-yash-vardhan.onrender.com",{
         query:{
             userId:authUser._id
         }
