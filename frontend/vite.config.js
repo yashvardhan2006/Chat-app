511c39e9aced65fd0eb9a943ef1fd4b5ebfc6067
yashvardhan2006 <yvardhansolanki@gmail.com>
2024-09-07 05:58:11 +0530

    Deploying

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "https://chat-app-yash-vardhan.onrender.com",
+				target: "http://localhost:8000",
 			},
 		},
 	},
